b3d1cb11091c1a439fdb662526984a10b1d62cf5
Thoại Phan <phantrongthoai2000@gmail.com>
2024-10-06 17:49:35 +0700

    Delete

--- StudenMVC/StudenMVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace StudenMVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
