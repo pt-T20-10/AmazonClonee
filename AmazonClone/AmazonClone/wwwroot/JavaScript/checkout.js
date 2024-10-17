0de9050be8f3cac76d9b7759f3e0078b6d173003
Thoại Phan <phantrongthoai2000@gmail.com>
2024-10-17 17:14:21 +0700

    Layout Admin + chỉnh hình part1

--- AmazonClone/AmazonClone/wwwroot/JavaScript/checkout.js
+++
@@ -1,6 +0,0 @@
-import { renderOrderSummary } from "./Checkout/orderSummary.js";
-import { renderPaymentSummary } from "./Checkout/paymentSummary.js";
-import { renderCheckoutHeader } from "./Checkout/checkoutHeader.js";
-renderCheckoutHeader();
-renderOrderSummary();
-renderPaymentSummary();
